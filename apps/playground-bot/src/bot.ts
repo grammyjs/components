f3de3eddd2a5a1133baeb899061f887121367c03
deptyped <deptyped@gmail.com>
2022-05-14 19:22:32 +0300

    feat(playground-bot): Add date, time, and color picker components

--- apps/playground-bot/src/bot.ts
+++ apps/playground-bot/src/bot.ts
@@ -1,50 +1,39 @@
 import { Bot, Keyboard } from "grammy";
-import { QrScanner } from "grammy-components";
+import {
+  ColorPicker,
+  QrScanner,
+  DatePicker,
+  TimePicker,
+} from "grammy-components";
 
 const bot = new Bot(""); // <-- put your authentication token between the ""
 
-bot.command("start", (ctx) => ctx.reply("Welcome!"));
+bot.command("start", (ctx) => {
+  const keyboard = new Keyboard();
 
-bot.command("setcommands", async (ctx) => {
-  await ctx.api.setMyCommands([
-    {
-      command: "qr",
-      description: "QR component",
+  keyboard.webApp("Date Picker", new DatePicker().build());
+  keyboard.webApp("Time Picker", new TimePicker().build()).row();
+  keyboard.webApp("Color Picker", new ColorPicker().build()).row();
+  keyboard.webApp("QR Scanner", new QrScanner().build()).row();
+
+  return ctx.reply("Welcome!", {
+    reply_markup: {
+      keyboard: keyboard.build(),
+      resize_keyboard: true,
     },
-  ]);
-  await ctx.reply("Commands updated");
+  });
 });
 
-bot
-  .on("message")
-  .on(":web_app_data", (ctx) => ctx.reply(ctx.message.web_app_data.data));
+bot.on("message").on(":web_app_data", (ctx) => {
+  const data = JSON.stringify(
+    JSON.parse(ctx.message.web_app_data.data),
+    null,
+    2
+  );
 
-bot.command("qr", (ctx) => {
-  ctx.reply("QR component", {
-    reply_markup: new Keyboard().webApp("Scan QR", new QrScanner().build()),
+  return ctx.reply("```\n" + data + "```", {
+    parse_mode: "MarkdownV2",
   });
-
-  // OR
-
-  /*
-  ctx.reply("QR component", {
-      reply_markup: new Keyboard().add(
-          new QrScanner().buildKeyboardButton("Scan QR")
-      ),
-  });
-  */
-
-  // OR
-
-  /*
-  ctx.reply("QR component", {
-      reply_markup: new Keyboard()
-          .add({
-              text: 'Scan QR',
-              web_app: new QrScanner()
-          }),
-  });
-  */
 });
 
 bot.start();
