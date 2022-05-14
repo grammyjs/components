5a53111370ccfc6ea38fb42b23ec51356acffb40
deptyped <deptyped@gmail.com>
2022-05-14 19:08:44 +0300

    feat(components): Add time picker component

--- packages/components/src/web/mod.ts
+++ packages/components/src/web/mod.ts
@@ -2,3 +2,4 @@ export * from "./component.ts";
 export * from "./components/qr-scanner.ts";
 export * from "./components/color-picker.ts";
 export * from "./components/date-picker.ts";
+export * from "./components/time-picker.ts";
