bc7d4916bf689ef57d2c06bb055565054831a8e4
deptyped <deptyped@gmail.com>
2022-05-15 12:22:35 +0300

    feat(web-components-app): Add animation of the appearance of components

--- apps/web-components-app/src/layouts/default.vue
+++ apps/web-components-app/src/layouts/default.vue
@@ -1,3 +1,21 @@
 <template>
-    <router-view></router-view>
+    <transition name="slide-up" appear>
+        <router-view />
+    </transition>
 </template>
+
+<style>
+.slide-up-enter-active {
+    transition: all 0.3s ease;
+}
+
+.slide-up-leave-active {
+    transition: all 0.3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+
+.slide-up-enter,
+.slide-fade-leave-to {
+    transform: translateY(5vh);
+    opacity: 0;
+}
+</style>
