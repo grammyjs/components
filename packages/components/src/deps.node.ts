f35508c2b7979d05917e67c1a36e367e7c47454b
deptyped <deptyped@gmail.com>
2022-08-09 12:16:28 +0300

    refactor(components): Use Telegram types from the core grammY package

--- packages/components/src/deps.node.ts
+++ packages/components/src/deps.node.ts
@@ -1,3 +1,3 @@
-export type { default as types } from "@grammyjs/types";
+export { InlineKeyboardButton, KeyboardButton } from "grammy/types";
 export { default as url } from "url";
 export * as grammy from "grammy";
